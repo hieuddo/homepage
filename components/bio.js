b990195e810dfbe7fe4799777ed3420bb1d3c2d3
hieuddo <hieu.dd.1998@gmail.com>
2023-07-22 16:05:25 +0800

    News flex-start

--- components/bio.js
+++ components/bio.js
@@ -2,8 +2,8 @@ import { Box } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 
 export const NewsSection = styled(Box)`
-  padding-left: 3.4em;
-  text-indent: -3.4em;
+  display: flex;
+  align-items: flex-start;
 `
 
 export const NewsDate = styled.span`
