a7c9bda6a2706948cd76397b1abf910b9b98a62d
hieuddo <hieu.dd.1998@gmail.com>
2023-07-23 23:38:42 +0800

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,39 @@
-import { ColorModeScript } from '@chakra-ui/react'
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import theme from '../lib/theme'
+import { ColorModeScript } from '@chakra-ui/react';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import theme from '../lib/theme';
 
 export default class Document extends NextDocument {
   render() {
+    const trackingId = process.env.GOOGLE_ANALYTICS_TRACKING_ID;
+
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {trackingId && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${trackingId}');
+                `,
+                }}
+              />
+            </>
+          )}
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
